18312743ed1ae9d2cb55e3f80c3e29f2f79deae7
MatiasG <matigodoy93@hotmail.com>
2024-05-16 23:30:07 -0300

    Validaciones

--- src/calendar/components/CalendarModal.tsx
+++ src/calendar/components/CalendarModal.tsx
@@ -1,9 +1,11 @@
 import { addHours, differenceInSeconds } from 'date-fns';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import Modal from 'react-modal';
 import DatePicker, { registerLocale } from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import { es } from 'date-fns/locale/es';
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 registerLocale('es', es);
 
@@ -30,6 +32,16 @@ export const CalendarModal = () => {
         start: new Date(),
         end: addHours(new Date(), 2)
     })
+    
+    const [formSubmitted, setFormSubmitted] = useState(false)
+
+    const titleClass = useMemo(() =>  {
+        if(!formSubmitted) return "";
+
+        return (formValues.title.length > 0) ? 'is-valid' : 'is-invalid';
+
+    }, [formValues.title, formSubmitted])
+
 
     const onInputChange = ({ target }) => {
         setFormValues({
@@ -52,15 +64,18 @@ export const CalendarModal = () => {
     
     const onSubmit = ( event ) => {
         event.preventDefault()
+        setFormSubmitted(true);
+
         const differenceBetweenDates = differenceInSeconds(formValues.end, formValues.start);
         
         // console.log(differenceBetweenDates);
 
         if(isNaN(differenceBetweenDates) || differenceBetweenDates <= 0) {
-            console.log('Error en fechas: No es un numero o la diferencia es menor a 0');
+            Swal.fire('Error en fechas', 'No es un numero o la diferencia es menor a 0', 'error');
             return;
         }
         if(formValues.title.length <= 0) {
+            Swal.fire('', '', 'error');
             return;
         }
         console.log(formValues);
@@ -83,7 +98,7 @@ export const CalendarModal = () => {
         <label>Fecha y hora inicio</label>
         <DatePicker
             selected={formValues.start}
-            className='form-control'
+            className={`form-control`}
             onChange={(event) => onDateChange(event, 'start')}
             dateFormat="Pp"
             showTimeSelect
@@ -111,7 +126,7 @@ export const CalendarModal = () => {
         <label>Titulo y notas</label>
         <input 
             type="text" 
-            className="form-control"
+            className={`form-control ${titleClass}`}
             placeholder="Título del evento"
             name="title"
             autoComplete="off"
@@ -123,7 +138,7 @@ export const CalendarModal = () => {
 
     <div className="form-group mb-2">
         <textarea 
-            className="form-control"
+            className={`form-control`}
             placeholder="Notas"
             rows={5}
             name="notes"
